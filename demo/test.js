21a01d07db6f202cae3467e012a676e168dda13a
ODudek <dudek.olek4@gmail.com>
2018-08-15 21:16:56 +0200

    eslint init

--- demo/test.js
+++ demo/test.js
@@ -1,3 +1,3 @@
-if ('tet') {
-    return test;
-}dsadsadasdsadasdsadas
+if (false) {
+	return;
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 (function() {
-    console.clear();
-    const fs = require('fs');
-    const chalk = require('chalk');
-    const config = require('./config');
-    
-    fs.watchFile(config.file, { interval: 1000 }, function() {
-        console.log('File Changed ...');
-        console.log('File content at : ' + new Date());
-    });
+	console.clear();
+	const fs = require('fs');
+	// const chalk = require('chalk');
+	const config = require('./config');
+	const path = require('path');
+	fs.watchFile(path.normalize(config.file), { interval: 1000 }, () => {
+		console.log('File Changed ...');
+		console.log('File content at : ' + new Date());
+	});
 })();
+
+// class Observer {
+// 	constructor(files) {
+// 		this.os = require('os');
+// 		this.fs = require('fs');
+// 		this.chalk = require('chalk');
+// 		this.path = require('path');
+// 		this.files = files;
+// 	}
+
+// 	isWindows() {
+// 		return this.os.platform() === 'win32' ? true : false;
+// 	}
+
+// 	convertPaths() {
+// 		if (this.isWindows()) {
+// 			this.path.join(__dirname)
+// 		}
+// 	}
+// }
+
+// new Observer()
